468055c19ee5e06767427655fe9536a94e7d598f
Kevin GANEM <Ganemkevin@gmail.com>
2022-03-02 14:50:45 +0100

    Formik & yup added

--- components/Form.js
+++ components/Form.js
@@ -8,89 +8,91 @@
 
 // REACT
 import React, { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
 // REACT-NATIVE
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
-import Modal from "react-native-modal";
+// FORM
+import { Formik } from "formik";
+import * as yup from "yup";
 
 export default function Form() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const isLogged = useState(false);
-  const {
-    control,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({ mode: "onBlur" });
+  const [isLogged, setIsLogged] = useState(false);
+
+  const reviewSchema = yup.object({
+    email: yup
+      .string()
+      .email("Invalid email")
+      .required("Required")
+      .min(6, "At least 6 characters"),
+    password: yup.string().required("Required").min(6, "At least 6 characters"),
+  });
 
   const onSubmit = (data) => {
     console.log(data);
-    isLogged(true);
+    setIsLogged((prev) => !prev);
   };
 
-  return (
+  return isLogged ? (
+    <View style={styles.centeredView}>
+      <Button title="Connect" color="red" onPressOut={handleSubmit(onSubmit)} />
+    </View>
+  ) : (
     <>
       <View style={styles.centeredView}>
-        <Controller
-          control={control}
-          name="Email"
-          render={({ field: { onChange, value, onBlur } }) => (
-            <TextInput
-              style={{
-                borderWidth: 1,
-                width: 200,
-                padding: 5,
-                borderRadius: 5,
-                borderColor: "lightgrey",
-              }}
-              placeholder="Enter your email"
-            />
-          )}
-          rules={{
-            required: {
-              value: true,
-              message: "Field is required!",
-            },
-            pattern: {
-              value: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$",
-              message: "Email invalid",
-            },
-          }}
-        />
-        <Controller
-          control={control}
-          name="Password"
-          render={({ field: { onChange, value, onBlur } }) => (
-            <TextInput
-              style={{
-                borderWidth: 1,
-                width: 200,
-                padding: 5,
-                borderRadius: 5,
-                borderColor: "lightgrey",
-              }}
-              type="password"
-              placeholder="Enter your password"
-            />
-          )}
-          rules={{
-            required: {
-              value: true,
-              message: "Field is required!",
-            },
-            pattern: {
-              value:
-                "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@[-`{-~]).{6,64}$",
-              message: "Password invalid",
-            },
-          }}
-        />
-
-        <Button
-          title="Connect"
-          color="#f194ff"
-          onPressOut={handleSubmit(onSubmit)}
-        />
+        <View style={styles.form}>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={reviewSchema}
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            {(props) => (
+              <View>
+                <Text style={styles.text}>E-mail</Text>
+                <View style={styles.input}>
+                  <TextInput
+                    style={{
+                      borderWidth: 1,
+                      width: 200,
+                      padding: 5,
+                      borderRadius: 5,
+                      borderColor: "lightgrey",
+                    }}
+                    placeholder="Enter your email"
+                    onChangeText={props.handleChange("email")}
+                    value={props.values.email}
+                  />
+                </View>
+                {props.touched.email && props.errors.email ? (
+                  <Text style={styles.textError}>{props.errors.email}</Text>
+                ) : null}
+                <Text style={styles.text}>Password</Text>
+                <View style={styles.input}>
+                  <TextInput
+                    style={{
+                      borderWidth: 1,
+                      width: 200,
+                      padding: 5,
+                      borderRadius: 5,
+                      borderColor: "lightgrey",
+                    }}
+                    placeholder="Enter your password"
+                    onChangeText={props.handleChange("password")}
+                    value={props.values.password}
+                  />
+                </View>
+                {props.touched.password && props.errors.password ? (
+                  <Text style={styles.textError}>{props.errors.password}</Text>
+                ) : null}
+                <Button
+                  title="Connect"
+                  color="blue"
+                  onPress={props.handleSubmit}
+                />
+              </View>
+            )}
+          </Formik>
+        </View>
       </View>
     </>
   );
@@ -99,5 +101,27 @@ export default function Form() {
 const styles = StyleSheet.create({
   centeredView: {
     alignItems: "center",
+  },
+  textError: {
+    color: "red",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  form: {
+    borderWidth: 1,
+    borderColor: "lightgrey",
+    padding: 10,
+  },
+  input: {
+    margin: 10,
+  },
+  text: {
+    marginTop: 10,
+    textAlign: "center",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  btn: {
+    width: 50,
   },
 });
